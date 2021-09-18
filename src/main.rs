656fd484be40f058827533ddb2aeea6fbf4e8f0a
Siarhei Isachanka <siarhei_isachanka@epam.com>
2021-09-18 18:01:05 +0200

    optimize output

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,21 @@ extern crate num_bigint;
 
 use num_bigint::BigUint;
 use std::env;
+use std::{thread, time};
+
+static mut COUNTER: u32 = 0;
+static MAX_DISPLAY_ITR: u32 = 25;
+static LAPTOP_SLEEP_SAVER: u64 = 1_000;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
-        println!("Application need a number as the first parameter.")
+        println!("Application need a number as a parameter.")
     } else {
-//        match args[1].parse::<u128>() {
-//            Err(_) => println!("{} is not a number", args[1]),
-//            Ok(lychrel) => {
-//                println!("init value: {}", lychrel);
-//                println!("result: {}", revert_str(lychrel));
-//            }
-//        }
-        match BigUint::parse_bytes(args[1].as_bytes(), 10){
+        match BigUint::parse_bytes(args[1].as_bytes(), 10) {
             None => println!("{} isn't a number", args[1]),
             Some(lychrel) => {
-                println!("result: {}", revert_str(lychrel));
+                println!("Result: {}", revert_str(lychrel));
             }
         }
     }
@@ -35,6 +33,20 @@ fn revert_str(dec: BigUint) -> BigUint {
     if dec == dec_rev {
         return dec;
     }
-    println!("{}",dec);
+    display(&dec);
     return revert_str(dec + dec_rev);
+}
+
+fn display(num: &BigUint) {
+    unsafe {
+        COUNTER += 1;
+        if COUNTER > MAX_DISPLAY_ITR {
+            if COUNTER % 100 == 0 {
+                thread::sleep(time::Duration::from_millis(LAPTOP_SLEEP_SAVER));
+            }
+            print!("\rIterations: {}", COUNTER);
+        } else {
+            println!("{}", num);
+        }
+    }
 }
